a56c5cb3c458f06110d719b4b3ae4e4c0f0f3dd8
Siarhei Sialitski <Siarhei.Sialitski@thomsonreuters.com>
2021-10-28 16:57:10 +0300

    Retry policy

--- src/OrderItemsReserver/OrderItemsReserver.cs
+++ src/OrderItemsReserver/OrderItemsReserver.cs
@@ -1,9 +1,11 @@
 using System;
 using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Polly;
 
 namespace OrderItemsReserver
 {
@@ -15,12 +17,42 @@ namespace OrderItemsReserver
             ILogger log)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
-
-            await outputBlob.CreateIfNotExistsAsync();
             
-            string blobName = DateTime.Now.ToString("MM_dd_yyyy/H:mm:ss");
-            var state = await outputBlob.UploadBlobAsync(blobName, BinaryData.FromString(myQueueItem));
-            log.LogInformation($"Order reserved: {myQueueItem} under {blobName}");
+            var policy = Policy.Handle<Exception>().WaitAndRetry(3,
+                attempt => TimeSpan.FromSeconds(0.1 * Math.Pow(2, attempt)),
+                (exception, calculatedWaitDuration) =>
+                {
+                    log.LogInformation($"exception: {exception.Message}");  
+                });
+            try
+            {
+                await policy.Execute(async () =>
+                {
+                    await outputBlob.CreateIfNotExistsAsync();
+
+                    throw new Exception("Manual exception");
+                    string blobName = DateTime.Now.ToString("MM_dd_yyyy/H:mm:ss");
+                    var state = await outputBlob.UploadBlobAsync(blobName, BinaryData.FromString(myQueueItem));
+                    log.LogInformation($"Order reserved: {myQueueItem} under {blobName}");
+                    
+                });        
+            }
+            catch (Exception e)
+            {
+                // Can't recover at this point
+                log.LogInformation($"critical error: {e.Message}");
+                var logicAppEndpoint = Environment.GetEnvironmentVariable("LogicAppEndpoint");
+                if (!string.IsNullOrEmpty(logicAppEndpoint))
+                {
+                    var client = new HttpClient();
+                    client.BaseAddress = new Uri(logicAppEndpoint);
+                    await client.PostAsync("", new StringContent(myQueueItem));
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
     }
 }
